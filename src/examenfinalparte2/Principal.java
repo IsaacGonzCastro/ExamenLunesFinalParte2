816d82b6bdedc93ab6a16b39f9a71059d669eb66
igonzalezcastro <igonzalezcastro@atanasoff08.danielcastelao.org>
2016-06-20 09:13:01 +0200

    Javadoc añadido.

--- src/examenfinalparte2/Principal.java
+++ src/examenfinalparte2/Principal.java
@@ -2,8 +2,17 @@ package examenfinalparte2;
 
 import java.util.Scanner;
 
+/**
+ * Introducimos un numero entero y el programa evalua si es primo o no.
+ *
+ * @author igonzalezcastro
+ */
 public class Principal {
 
+    /**
+     * variable que devuelve true si el numero es primo y false si el numero no
+     * lo es.
+     */
     public static boolean esPrimo = false;
 
     public static void main(String arg[]) {
@@ -41,6 +50,15 @@ public class Principal {
         }
     }
 
+    /**
+     * Este metodo calcula el numero de divisores.
+     *
+     * @param i es el divisor.
+     * @param limite es el limite de divisores.
+     * @param comparador es el numero del que se van a contar los divisores.
+     * @param countDivisores es el contador de los divisores
+     * @return devuelve el comparador si es primo o no.
+     */
     private static boolean calcularDivisores(int i, int limite, int comparador, int countDivisores) {
         boolean esPrimo = false;
         while (i <= limite) {
@@ -60,6 +78,13 @@ public class Principal {
         return esPrimo;
     }
 
+    /**
+     * Este metodo cuenta las divisiones que se pueden con el numero que recibe
+     * por parametros.
+     *
+     * @param comparador es el numero del que se van a contar los divisores.
+     * @return devuelve el numero de divisiones que se pueden hacer.
+     */
     private static int contarDivisiones(int comparador) {
         int aux = comparador;
         int contador = 0;
@@ -70,6 +95,11 @@ public class Principal {
         return contador;
     }
 
+    /**
+     * Introduce el numero entero por teclado.
+     *
+     * @return devuelve el numero que se introdujo por teclado.
+     */
     public static int introducirNumDigito() {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce un dígito:");
