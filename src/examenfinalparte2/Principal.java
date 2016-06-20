a32c0f15eb05b6f69019a2833ed7e4540093e9fa
igonzalezcastro <igonzalezcastro@atanasoff08.danielcastelao.org>
2016-06-20 09:09:42 +0200

    Metodos añadidos y variables renombradas.

--- src/examenfinalparte2/Principal.java
+++ src/examenfinalparte2/Principal.java
@@ -1,60 +1,78 @@
-
 package examenfinalparte2;
+
+import java.util.Scanner;
 
 public class Principal {
 
-    public static boolean p = false;
-    
-    public static void main(String arg[])
-        {
-        int dig=2;
-        int ndig=0;
-        if(dig<=0)
-        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        for(int i = 1; i <= 99999; i++ )
-        {
-            int aux = i;
- 
-            int contador=0;
- 
-            while (aux != 0)
-        {
+    public static boolean esPrimo = false;
+
+    public static void main(String arg[]) {
+        int dig;
+        int ndig;
+        do {
+            dig = introducirNumDigito();
+            if (dig <= 0) {
+                System.out.println("Ingrese como parámetro, un numero de digito correcto (mayor que 0): ");
+            }
+        } while (dig <= 0);
+        for (int comparador = 1; comparador <= 99999; comparador++) {
+            ndig = contarDivisiones(comparador);
+
+            if (ndig == dig) {
+                if (comparador < 4) {
+                    esPrimo = true;
+                } else if (comparador % 2 == 0) {
+                    esPrimo = false;
+                } else {
+                    int countDivisores = 0;
+                    int i = 1;
+                    int limite = (comparador - 1) / 2;
+                    if (limite % 2 == 0) {
+                        limite--;
+                    }
+
+                    esPrimo = calcularDivisores(i, limite, comparador, countDivisores);
+                }
+
+                if (esPrimo == true) {
+                    System.out.println(comparador);
+                }
+            }
+        }
+    }
+
+    private static boolean calcularDivisores(int i, int limite, int comparador, int countDivisores) {
+        boolean esPrimo = false;
+        while (i <= limite) {
+            if (comparador % i == 0) {
+                countDivisores++;
+            }
+            i += 2;
+            if (countDivisores == 2) {
+                i = limite + 1;
+            }
+        }
+
+        if (countDivisores == 1) {
+            esPrimo = true;
+        }
+
+        return esPrimo;
+    }
+
+    private static int contarDivisiones(int comparador) {
+        int aux = comparador;
+        int contador = 0;
+        while (aux != 0) {
             aux = aux / 10;
             contador++;
         }
-        ndig=contador;
-            
-            
-            if(ndig==dig){
-                   if (i < 4) p = true;
-            else
-            {
-            if (i % 2 == 0) p = false; 
-                else
-                {
-                    int contador1 = 0;  
-                    int i1 = 1; 
-                    int k = (i - 1) / 2; 
-                    if  (k % 2 == 0) k--;  
-             
-                    while(i1 <= k)
-                    {
-                        if (i % i1 == 0) contador1++;
-                            i1 += 2;
-                            if (contador1 == 2) i1 = k + 1;
-                    }
- 
-        if (contador1 == 1) p = true;
-            } 
-        } 
-     
-                if (p == true) 
-                    System.out.println(i);    
-            }
-        }
-        }
- 
-       
- 
-    
+        return contador;
+    }
+
+    public static int introducirNumDigito() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce un dígito:");
+        return teclado.nextInt();
+    }
 }
